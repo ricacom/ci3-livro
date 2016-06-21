69dc0402dfa055def89c357b99a9b0a5ac724b8b
Ricardo Costa Sousa <rsousa@defensoria.sp.gov.br>
2016-06-21 16:53:48 -0300

    until page 74

--- site-institucional/application/config/routes.php
+++ site-institucional/application/config/routes.php
@@ -55,4 +55,8 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 
-//$route['product/:num'] = 'example/login';
+$route['empresa'] = 'Institucional/Empresa';
+$route['servicos'] = 'Institucional/Servicos';
+
+$route['fale-conosco'] 		= 'Contato/FaleConosco';
+$route['trabalhe-conosco'] 	= 'Contato/TrabalheConosco';
